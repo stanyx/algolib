c8ea841a70fb42b62e671a5a3a2071ad09d3124d
alexn <thinkandwin5000@gmail.com>
2016-10-02 00:35:55 +0300

    cocktail sort

--- sort/cocktail/main.go
+++ sort/cocktail/main.go
@@ -1,1 +1,59 @@
 package main
+
+func CocktailSort(A []int) []int {
+
+	left := 0
+	right := len(A) - 1
+	swaps := 0
+	iters := 0
+	
+	for {
+
+		j := left
+
+		for {
+
+			if j >= right {
+				break
+			}
+
+			if (A[j] > A[j + 1]) {
+				t := A[j + 1]
+				A[j + 1] = A[j]
+				A[j] = t
+				swaps += 1
+			}
+
+			j++
+			iters += 1
+		}
+
+		i := right
+
+		for {
+
+			if i <= left {
+				break
+			}
+
+			if (A[i] < A[i - 1]) {
+				t := A[i - 1]
+				A[i - 1] = A[i]
+				A[i] = t
+				swaps += 1
+			}
+			
+			i--
+			iters += 1
+		}
+
+		left += 1
+		right -= 1
+
+		if left > right {
+			break
+		}
+	}
+
+	return A
+}
