568c242bd6b68cf4e14fb0fc33e02a6bb30c8566
alex <thinkandwin5000@gmail.com>
2020-04-21 00:38:00 +0300

    refactor insertion sort

--- sort/insertion/main.go
+++ sort/insertion/main.go
@@ -1,19 +1,19 @@
 package insertion
 
+import "fmt"
 
+// InsertionSort sorts the input slice of integers in ascending order
+// Worst-case complexity is O(N^2)
+// Best-case complexity is O(N)
 func InsertionSort(A []int) {
-
+	fmt.Println("INSERTION SORT ALGORITHM")
 	for i := 1; i < len(A); i++ {
 		key := A[i]
-		lst := i
-		for j := i - 1; j > -1; j-- {
-			if A[j] > key {
-				A[j + 1] = A[j]
-				lst = j
-			} else {
-				break
-			}
+		insertionIndex := i
+		for j := i - 1; j >= 0 && A[j] > key; j-- {
+			A[j+1] = A[j]
+			insertionIndex = j
 		}
-		A[lst] = key
+		A[insertionIndex] = key
 	}
 }
